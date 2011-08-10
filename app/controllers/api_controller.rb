18a2874bd6da2714fae6f3857feb5775fd098e73
Alex Koppel <alex+git@alexkoppel.com>
2011-08-10 12:03:40 +0200

    Properly detect prior runs.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   def start_run
     # start a test run if there hasn't been one in the last half hour
-    unless last_run = TestRun.where("created_at < ?", Time.now - 30.minutes)
+    unless last_run = TestRun.where("created_at < ?", Time.now - 30.minutes).limit(1).first
       system("bundle exec rake fb_tests:run")
       @result = :started
     else
