fee3ee4b08700ea49b453e604f282586447b28e1
Alex Koppel <alex+git@alexkoppel.com>
2011-08-10 14:15:27 +0200

    Added more logging.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,15 @@
 class ApiController < ApplicationController
   def start_run
     # start a test run if there hasn't been one in the last half hour
-    unless last_run = TestRun.where("created_at < ?", Time.now - 30.minutes).limit(1).first
+    logger.info("Starting!")
+    last_run = TestRun.where("created_at < ?", Time.now - 30.minutes).limit(1).first
+    logger.debug("Found #{last_run.inspect}")
+    unless last_run
       logger.info("Starting tests.")
       system("bundle exec rake fb_tests:run")
       @result = :started
     else
+      logger.debug("Too soon :(")
       @result = :too_soon
     end
     render :json => {:status => @result}
