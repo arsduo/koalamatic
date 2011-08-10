91421a24c4f4f0810042159c468d3a025b4d2bb8
Alex Koppel <alex+git@alexkoppel.com>
2011-08-10 13:06:30 +0200

    Added flag so the start of a test run is visible in the log.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,7 @@ class ApiController < ApplicationController
   def start_run
     # start a test run if there hasn't been one in the last half hour
     unless last_run = TestRun.where("created_at < ?", Time.now - 30.minutes).limit(1).first
+      logger.info("Starting tests.")
       system("bundle exec rake fb_tests:run")
       @result = :started
     else
