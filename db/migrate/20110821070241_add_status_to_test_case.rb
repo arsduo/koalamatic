c2f78512e7db6a5a37dd1eeaf832d6589ef50c76
Alex Koppel <alex+git@alexkoppel.com>
2011-09-07 11:36:12 +0200

    Made migration compatible with Postgres.

--- db/migrate/20110821070241_add_status_to_test_case.rb
+++ db/migrate/20110821070241_add_status_to_test_case.rb
@@ -3,9 +3,9 @@ class AddStatusToTestCase < ActiveRecord::Migration
 
   def change
       # test cases either pass or fail
-      add_column :test_cases, :failed, :boolean
+      #add_column :test_cases, :failed, :boolean
       
       # update all existing test runs to failures
-      TestCase.update_all("failed = 1")
+      TestCase.update_all("failed = true")
   end
 end
