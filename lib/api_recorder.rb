46f5f6a8e62609d18a2d762b8aa48c6f3fb9c97e
Alex Koppel <alex+git@alexkoppel.com>
2011-09-26 12:40:54 +0200

    Fix for ApiRecorder.run.

--- lib/api_recorder.rb
+++ lib/api_recorder.rb
@@ -3,7 +3,9 @@ class ApiRecorder < Faraday::Middleware
   # there's only one test run happening at any time on any Koalamatic instance
   # (though there may be many test cases going in parallel)
   # so we can set this as a class variable without having to worry about threads
-  attr_accessor :run
+  class << self
+    attr_accessor :run
+  end
   
   def call(env)
     # read the request body first, since Faraday replaces it with the response
@@ -30,7 +32,7 @@ class ApiRecorder < Faraday::Middleware
     end
     # if we're inside a test run,
     # don't count time spent writing to the database toward total test execution
-    run ? run.without_recording_time(&record_call) : record_call.call
+    ApiRecorder.run ? ApiRecorder.run.without_recording_time(&record_call) : record_call.call
     
     # pass it on to the next middleware
     result
