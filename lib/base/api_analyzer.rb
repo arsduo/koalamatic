5043700e12c52a520919f7777281ff41886b5eae
Alex Koppel <alex+git@alexkoppel.com>
2011-10-24 22:48:34 +0200

    Prototyping the analyzer
    Some very rough prototype designs in preparation for test-writing.

---
+++ lib/base/api_analyzer.rb
@@ -0,0 +1,39 @@
+require 'base/api_interaction'
+module Koalamatic
+  module Base
+    class ApiAnalyzer
+      def self.interaction_class
+        Koalamatic::Base::ApiInteraction
+      end
+    end
+    
+    class ApiMatcher
+      @matchers = {}
+      
+      def self.match?(interaction)
+        @matchers.each_pair do |attribute, match_data|
+          # evaluate each of the declared matchers, aborting the match if it returns false
+          return false unless evaluate_match(interaction.send(attribute), match_data)
+        end
+        # if all the matchers returned true, we have a match
+        true
+      end
+      
+      def self.method_missing(name, *args, &block)
+        if interaction_class.column_names.include?(name.to_s)
+          # we're matching against a column
+          @matchers[name] = {:args => args, :block => block}
+        else
+          super
+        end
+      end
+      
+      protected
+      
+      def self.evaluate_match(value, match_data)
+        valid = if match_data[:block]
+          match_data.c
+      end
+    end
+  end
+end
